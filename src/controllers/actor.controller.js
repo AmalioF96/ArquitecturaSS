67b6abf90a6192c1dcf8e601b117aaf01b0e74be
José Manuel Muñiz Peña <josemamup@gmail.com>
2022-02-21 20:06:29 +0100

    Generación de password con middleware

--- src/controllers/actor.controller.js
+++ src/controllers/actor.controller.js
@@ -21,16 +21,15 @@ exports.list_all_actors = function (req, res) {
   })
 }
 
-// TODO: Código de álvaro BORRAR?
 // exports.create_an_actor = function (req, res) {
 //   const newActor = new Actor(req.body)
 //   // If new_actor is a customer, validated = true;
 //   // If new_actor is a clerk, validated = false;
-//   if ((newActor.role.includes('ADMINISTRATOR'))) {
-//     newActor.validated = false
-//   } else {
-//     newActor.validated = true
-//   }
+//   // if ((newActor.role.includes('ADMINISTRATOR'))) {
+//   //   newActor.validated = false
+//   // } else {
+//   //   newActor.validated = true
+//   // }
 //   newActor.save(function (err, actor) {
 //     if (err) {
 //       res.send(err)
@@ -40,29 +39,47 @@ exports.list_all_actors = function (req, res) {
 //   })
 // }
 
+// exports.create_an_actor = function (req, res) {
+//   bcrypt.genSalt(6, function(err, salt) {
+//     var newActor = new Actor(req.body)
+//     if (err) {
+//       res.status(500).send(err)
+//     }
+//     bcrypt.hash(req.body.password, salt, function(err, hash) {
+//       newActor.password = hash
+//       newActor.save(function (err, actor) {
+//         if (err) {
+//           if (err.name === 'ValidationError') {
+//             res.status(422).send(err)
+//           } else {
+//             res.status(500).send(err)
+//           }
+//         } else {
+//           res.json(actor)
+//         }
+//       })
+//     })
+//   })
+// }
+
+
 exports.create_an_actor = function (req, res) {
-  bcrypt.genSalt(6, function(err, salt) {
-    var newActor = new Actor(req.body)
+  const newActor = new Actor(req.body)
+  // If new_actor is a customer, validated = true;
+  // If new_actor is a clerk, validated = false;
+
+  newActor.save(function (err, actor) {
     if (err) {
-      res.status(500).send(err)
+      if (err.name === 'ValidationError') {
+        res.status(422).send(err)
+      } else {
+        res.status(500).send(err)
+      }
+    } else {
+      res.json(actor)
     }
-    bcrypt.hash(req.body.password, salt, function(err, hash) {
-      newActor.password = hash
-      newActor.save(function (err, actor) {
-        if (err) {
-          if (err.name === 'ValidationError') {
-            res.status(422).send(err)
-          } else {
-            res.status(500).send(err)
-          }
-        } else {
-          res.json(actor)
-        }
-      })
-    })
   })
 }
-
 
 exports.read_an_actor = function (req, res) {
   Actor.findById(req.params.actorId, function (err, actor) {
@@ -82,36 +99,53 @@ function compare(encrypted) {
   })
 }
 
+// exports.update_an_actor = function (req, res) {
+//   // Check that the user is the proper actor and if not: res.status(403);
+//   // "an access token is valid, but requires more privileges"
+
+//   const newPassword = req.body.password
+//   console.log("newPass", newPassword)
+//   const actorBody = req.body
+
+//   var salt = bcrypt.genSaltSync(6);
+//   var hashi = bcrypt.hash(newPassword, salt, (err, resHash) => {
+//     console.log('hash', resHash)
+//     var hash = resHash
+//     bcrypt.compare(newPassword, hash, (err, resHash) => {
+//       console.log('Compared result', resHash, hash) 
+//       if (resHash==true) {
+//         //Se ha modificado la contraseña
+//         console.log("La contraseña ha cambiado")
+//         console.log("Contra antes", actorBody.password)
+//         req.body.password = hash
+//         console.log("Contra después", actorBody.password)
+        
+//       } else {
+//         //No se ha modificado la contraseña
+//         console.log("La contraseña no ha cambiado")
+//       }
+//       console.log('FINAL', req.body.password)
+      
+//       Actor.findOneAndUpdate({ _id: req.params.actorId }, req.body, { new: true }, function (err, actor) {
+//         if (err) {
+//           if (err.name === 'ValidationError') {
+//             res.status(422).send(err)
+//           } else {
+//             res.status(500).send(err)
+//           }
+//         } else {
+//           console.log('CHECK PASS FINAL', req.body.password)
+//           res.json(actor)
+//         }
+//       })
+//     })
+  // }); 
+// }
+
 exports.update_an_actor = function (req, res) {
   // Check that the user is the proper actor and if not: res.status(403);
   // "an access token is valid, but requires more privileges"
-
-  const newPassword = req.body.password
-  console.log("newPass", newPassword)
-  const actorBody = req.body
-
-  var salt = bcrypt.genSaltSync(6);
-  bcrypt.hash(newPassword, salt, (err, res) => {
-    console.log('hash', res)
-    var hash = res
-    bcrypt.compare(newPassword, hash, (err, res) => {
-      console.log('Compared result', res, hash) 
-      if (res==true) {
-        //Se ha modificado la contraseña
-        console.log("La contraseña ha cambiado")
-        console.log("Contra antes", actorBody.password)
-        req.body.password = hash
-        console.log("Contra después", actorBody.password)
-      } else {
-        //No se ha modificado la contraseña
-        console.log("La contraseña no ha cambiado")
-      }
-    })
-  }); 
-
-
-
-  Actor.findOneAndUpdate({ _id: req.params.actorId }, actorBody, { new: true }, function (err, actor) {
+  Actor.findOneAndUpdate({ _id: req.params.actorId }, req.body, { new: true }, function (err, actor) {
     if (err) {
       if (err.name === 'ValidationError') {
         res.status(422).send(err)
@@ -123,8 +157,6 @@ exports.update_an_actor = function (req, res) {
     }
   })
 }
-
-
 
 exports.delete_an_actor = function (req, res) {
     res.send('ERROR');
--- src/entities/actor.entity.js
+++ src/entities/actor.entity.js
@@ -2,6 +2,8 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const bcrypt = require('bcrypt')
+
 const ActorSchema = new Schema({
     name: { type: String, required: true },
     surname: { type: String, required: true },
@@ -15,5 +17,44 @@ const ActorSchema = new Schema({
     isDeleted: { type: Boolean, default: false}
 });
 
+ActorSchema.pre('save', function (callback) {
+    const actor = this
+    // Break out if the password hasn't changed
+    // if (!actor.isModified('password')) return callback()
+  
+    // Password changed so we need to hash it
+    bcrypt.genSalt(5, function (err, salt) {
+      if (err) return callback(err)
+  
+      bcrypt.hash(actor.password, salt, function (err, hash) {
+        if (err) return callback(err)
+        actor.password = hash
+        callback()
+      })
+    })
+  })
+  
+  ActorSchema.pre('findOneAndUpdate', function (callback) {
+    const actor = this._update
+  
+    bcrypt.genSalt(5, function (err, salt) {
+      if (err) return callback(err)
+  
+      bcrypt.hash(actor.password, salt, function (err, hash) {
+        if (err) return callback(err)
+        actor.password = hash
+        callback()
+      })
+    })
+  })
+  
+  ActorSchema.methods.verifyPassword = function (password, cb) {
+    bcrypt.compare(password, this.password, function (err, isMatch) {
+      // console.log('verifying password in actorModel: ' + password)
+      if (err) return cb(err)
+      // console.log('iMatch: ' + isMatch)
+      cb(null, isMatch)
+    })
+  }
 
 module.exports = mongoose.model('Actors', ActorSchema)
